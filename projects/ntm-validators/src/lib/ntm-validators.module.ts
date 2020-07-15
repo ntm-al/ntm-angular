4326827569cf2952ff0866dea9c06f7b805c0861
Wellici Araujo <wja1@ifal.edu.br>
2020-07-15 10:00:47 -0300

    fix: export zip-code directive

--- projects/ntm-validators/src/lib/ntm-validators.module.ts
+++ projects/ntm-validators/src/lib/ntm-validators.module.ts
@@ -7,6 +7,7 @@ import { TwitterMaskDirective } from './directives/twitter-mask.directive';
 import { DifferenceTimePipe } from './pipes/difference-time.pipe';
 import { NumberToMonthPipe } from './pipes/number-to-month.pipe';
 import { FirstAndLastNamePipe } from './pipes/first-and-last-name.pipe';
+import { ZipCodeMaskDirective } from './directives/zip-code-mask.directive';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { FirstAndLastNamePipe } from './pipes/first-and-last-name.pipe';
     DifferenceTimePipe,
     FirstAndLastNamePipe,
     NumberToMonthPipe,
+    ZipCodeMaskDirective,
   ],
   imports: [],
   exports: [
@@ -29,6 +31,7 @@ import { FirstAndLastNamePipe } from './pipes/first-and-last-name.pipe';
     DifferenceTimePipe,
     FirstAndLastNamePipe,
     NumberToMonthPipe,
+    ZipCodeMaskDirective,
   ],
 })
 export class NtmValidatorsModule {}
