40fab5f31533d7e47f92a1913450db66cab3f75f
Roberto <robertojf95@gmail.com>
2020-08-02 14:09:43 -0300

    fix twitter mask

--- projects/core/src/lib/directives/twitter-mask.directive.spec.ts
+++ projects/core/src/lib/directives/twitter-mask.directive.spec.ts
@@ -1,18 +1,32 @@
 import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TwitterMaskDirective } from './twitter-mask.directive';
+import { By } from '@angular/platform-browser';
 
 @Component({
-  template: '<input type="text" ntmTwitterMask>',
+  template: '<input ntmTwitterMask>',
 })
 class TestTwitterMaskDirectiveComponent {}
 
 describe('TwitterMaskDirective', () => {
   let fixture: ComponentFixture<TestTwitterMaskDirectiveComponent>;
+  let inputEl: HTMLInputElement;
   beforeEach(() => {
-    fixture = TestBed.configureTestingModule({
-      declarations: [TestTwitterMaskDirectiveComponent],
-    }).createComponent(TestTwitterMaskDirectiveComponent);
+    TestBed.configureTestingModule({
+      declarations: [TestTwitterMaskDirectiveComponent, TwitterMaskDirective],
+    });
+    fixture = TestBed.createComponent(TestTwitterMaskDirectiveComponent);
+    inputEl = fixture.debugElement.query(By.css('input')).nativeElement as HTMLInputElement;
+  });
+
+  it('should apply mask for twitter', () => {
+    const arr = {
+      input: '@úser',
+      expect: '@ser',
+    };
+    inputEl.value = arr.input;
+    inputEl.dispatchEvent(new Event('input'));
+    void expect(inputEl.value).toBe(arr.expect);
   });
 
   it('should return correct formated twitter user string', () => {
