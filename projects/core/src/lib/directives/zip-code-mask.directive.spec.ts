30431e39e0d7e302db70996f91b257eb2658e5fe
Roberto <robertojf95@gmail.com>
2020-08-02 14:02:30 -0300

    fix zipcode test

--- projects/core/src/lib/directives/zip-code-mask.directive.spec.ts
+++ projects/core/src/lib/directives/zip-code-mask.directive.spec.ts
@@ -1,18 +1,42 @@
 import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ZipCodeMaskDirective } from './zip-code-mask.directive';
+import { By } from '@angular/platform-browser';
 
 @Component({
-  template: '<input type="text" appZipCodeMask>',
+  template: '<input type="text" ntmZipCodeMask>',
 })
 class TestZipCodeMaskDirectiveComponent {}
 
 describe('ZipCodeMaskDirective', () => {
   let fixture: ComponentFixture<TestZipCodeMaskDirectiveComponent>;
+  let inputEl: HTMLInputElement;
   beforeEach(() => {
-    fixture = TestBed.configureTestingModule({
-      declarations: [TestZipCodeMaskDirectiveComponent],
-    }).createComponent(TestZipCodeMaskDirectiveComponent);
+    TestBed.configureTestingModule({
+      declarations: [TestZipCodeMaskDirectiveComponent, ZipCodeMaskDirective],
+    });
+    fixture = TestBed.createComponent(TestZipCodeMaskDirectiveComponent);
+    inputEl = fixture.debugElement.query(By.css('input')).nativeElement as HTMLInputElement;
+  });
+  it('should apply mask for zipcode', () => {
+    const arr = {
+      input: '57072120',
+      expect: '57072-120',
+    };
+    inputEl.value = arr.input;
+    inputEl.dispatchEvent(new Event('input'));
+    void expect(inputEl.value).toBe(arr.expect);
+  });
+
+  it('should not apply mask for not valid zipcode', () => {
+    const arr = {
+      input: 'ss-$sdf',
+      expect: '',
+    };
+
+    inputEl.value = arr.input;
+    inputEl.dispatchEvent(new Event('input'));
+    void expect(inputEl.value).toBe(arr.expect);
   });
 
   it('should return correct formated zip-code string', () => {
